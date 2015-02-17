4ac604f8ea7709c763ca9b11d6c762a4407a7a5b
svirdi <sanvir1982@gmail.com>
2015-02-16 21:37:40 -0800

    fix link

--- public/javascripts/pagination.js
+++ public/javascripts/pagination.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
     console.log( "ready!" );
     console.log( "url" +window.location.href ); 
-    if (window.location.href.indexOf("/posts/") > -1) {
+    if ((window.location.href.indexOf("/posts/") > -1) || (window.location.href.indexOf("/about") > -1))  {
         console.log("");
        $( ".pager" ).remove()
     }
