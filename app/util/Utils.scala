f584a8fbde2bbf550739cde44b1a5b6f414c16f0
svirdi <sanvir1982@gmail.com>
2015-02-17 20:49:19 -0800

    change location of user properties file for prod

--- app/util/Utils.scala
+++ app/util/Utils.scala
@@ -1,12 +1,15 @@
 package util
 
 import scala.io.Source
-
+import play.api.Play
 /**
  * Created by svirdi on 1/11/15.
  */
 object Utils {
 
-  val userName = Source.fromFile("conf/user.properties") getLines() find(_.startsWith("username")) map(_.replace("username=",""))
+  val userName = if (Play.current.mode == play.api.Mode.Prod)
+    Source.fromFile("/home/sandeep/user.properties") getLines() find(_.startsWith("username")) map(_.replace("username=",""))
+  else
+      Source.fromFile("conf/user.properties") getLines() find(_.startsWith("username")) map(_.replace("username=",""))
 
 }
