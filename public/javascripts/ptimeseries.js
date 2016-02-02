a55f287d8a20cc1d39d4e769364e41b548181942
sandeep virdi <code.monkey.1982@gmail.com>
2016-02-01 17:28:04 -0800

    search form ajax

--- public/javascripts/ptimeseries.js
+++ public/javascripts/ptimeseries.js
@@ -6,17 +6,19 @@ $( document ).ready(function(){
       month = $(event.target).text();
       $( "#month :button" ).text(month);
     }); 
-    $( "#gevent li a" ).click(function(event){
-      console.log("Event Text " +$(event.target).text());
-      etype = $(event.target).text();
-      $( "#gevent :button" ).text(etype);
-    }); 
 
     $( "#tform" ).click(function(){
-      if ( month === undefined || etype === undefined || $( "#rname" ).val().length < 3) {
+      if ( month === undefined || $( "#rname" ).val().length < 3) {
         alert("Fill out complete form before sumbit !!!!")
       } else {
-         
+        $.ajax({
+          type: 'GET',
+          url: '/svirdi/searchts/' +encodeURIComponent($( "#rname" ).val()) +'/' + month,
+          data: 'json',
+          success: function(data) {
+            console.log("Search Data "+JSON.stringify(data));
+          }
+        }); 
       } 
     });
 
