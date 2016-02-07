288e7202cdb110d0656905bb8d82c43242c3f4b3
sandeep virdi <code.monkey.1982@gmail.com>
2016-02-06 23:49:00 -0800

    remove dummy data

--- public/javascripts/useractivity.js
+++ public/javascripts/useractivity.js
@@ -7,7 +7,7 @@ $( document ).ready(function(){
             format: 'json'
           },
           success: function(data) {
-             //console.log("Pie Data "+JSON.stringify(data));
+             console.log("Pie Data "+JSON.stringify(data));
 
              nv.addGraph(function() {
                   var chart = nv.models.pieChart()
@@ -25,44 +25,5 @@ $( document ).ready(function(){
           },
           type: 'GET'
           });
-
-
-    //Pie chart example data. Note how there is only a single array of key-value pairs.
-    function exampleData() {
-      return  [
-          {
-            "label": "One",
-            "value" : 29.765957771107
-          } ,
-          {
-            "label": "Two",
-            "value" : 0
-          } ,
-          {
-            "label": "Three",
-            "value" : 32.807804682612
-          } ,
-          {
-            "label": "Four",
-            "value" : 196.45946739256
-          } ,
-          {
-            "label": "Five",
-            "value" : 0.19434030906893
-          } ,
-          {
-            "label": "Six",
-            "value" : 98.079782601442
-          } ,
-          {
-            "label": "Seven",
-            "value" : 13.925743130903
-          } ,
-          {
-            "label": "Eight",
-            "value" : 5.1387322875705
-          }
-        ];
-    }
    }
 });
